055edfe7a5d3b4cbf65047bdf0f6a884e538392a
Duy-Hieu Bui <hieubd@vnu.edu.vn>
2021-01-26 14:45:09 +0700

    - Clean up unnecessary files

--- generated/DeviceUnderTest.v
+++
@@ -1,9 +0,0 @@
-module DeviceUnderTest(
-  input        clock,
-  input        reset,
-  input  [4:0] io_a,
-  input  [4:0] io_b,
-  output [4:0] io_out
-);
-  assign io_out = io_a & io_b; // @[DeviceUnderTest.scala 12:10]
-endmodule
